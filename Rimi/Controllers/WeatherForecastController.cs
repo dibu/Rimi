0afe884a0ed6d6c9f8ed045e355157367c70ff1d
Dibyajyoti Das <dibu.aspnet@hotmail.com>
2021-08-29 12:09:12 +0530

    comment added

--- Rimi/Controllers/WeatherForecastController.cs
+++ Rimi/Controllers/WeatherForecastController.cs
@@ -12,7 +12,9 @@ namespace Rimi.Controllers {
     public class WeatherForecastController : ControllerBase {
         private readonly ITestClass1 testClass1;
         private readonly ITestClass2 testClass2;
+        /* dependency kikore register kore , Startup.cs file e dekh */
         /* constructor er vetor interface guloke pass korbi, n number interface pass korte parbi */
+
         public WeatherForecastController(ITestClass1 class1, ITestClass2 class2/* , Interface .... nth */) {
             this.testClass1 = class1;
             this.testClass2 = class2;
